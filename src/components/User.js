c29c544069ab2ea13cb638651ebe3723285e1b0f
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-05-11 16:29:21 +0300

    React Event

--- src/components/User.js
+++ src/components/User.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 
 class User extends Component {
+
     constructor(probs){
         super(probs);
         this.state = {
-            isvisible : false
+            isvisible : true
         }
+    }
+    onClickEvent(e){
+        console.log(e.target);
+        console.log("test");
+        console.log(this);
     }
     render() {
         // Destructing
         const {name, department, salary} = this.props;
-        const {isvisible} = this.state;
+        const {isvisible, abc} = this.state;
         return (
             <div className = "col-md-8 mb-4">
                 <div className = "card">
                     <div className="card-header d-flex justify-content-between">
-                        <h4>İsim : {name} <i className="fa fa-check-square" aria-hidden="true"></i></h4>
+                        <h4 className = "d-inline" onClick = {this.onClickEvent}>İsim : {name} <i className="fa fa-check-square" aria-hidden="true" ></i></h4>
                         <i className = "far fa-trash-alt" styele = {{cursor: "pointer"}}></i>
                     </div>
                 </div>
