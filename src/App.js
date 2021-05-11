69ed4d93970abeffc8fb2b318e969473fac6cb9e
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-05-11 11:03:51 +0300

    bismillah

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>merhaba</h1>
+      <h1>buraya sadece bir tane div yazabilirsin</h1>
+    </div>
+  );
+}
+
+export default App;
