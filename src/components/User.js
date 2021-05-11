9f43d28928f7df8ff6fb7e14561a01da731737ba
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-05-11 16:05:41 +0300

    Cart object futures

--- src/components/User.js
+++ src/components/User.js
@@ -6,13 +6,19 @@ class User extends Component {
         // Destructing
         const {name, department, salary} = this.props;
         return (
-            <div>
-                <ul>
-                    <li>İsim : {name} <i className="fa fa-check-square" aria-hidden="true"></i></li>
-                    <li>Departman : {department} <i className="fa fa-window-close-o" aria-hidden="true"></i>
-</li>
-                    <li>Maaş : {salary}</li>
-                </ul>
+            <div className = "col-md-8 mb-4">
+                <div className = "card">
+                    <div className="card-header d-flex justify-content-between">
+                        <h4>İsim : {name} <i className="fa fa-check-square" aria-hidden="true"></i></h4>
+                        <i className = "far fa-trash-alt" styele = {{cursor: "pointer"}}></i>
+                    </div>
+                </div>
+
+                <div className="card-body">
+                    <p className="card-text">Departman : {department}</p>
+                    <p className="card-text">Salary : {salary}</p>
+                </div>
+
             </div>
         )
     }
